03be31ffcdd324629ddb9ce9cc0bea7ac52cfb0a
scottc522 <b4005596@my.shu.ac.uk>
2018-01-16 14:56:51 +0000

    Finished Commenting, Might come back and find way to terminate whole thing at somepoint. Maybe by having a channel all the way back to the start.

--- CPUSimulator.go
+++ CPUSimulator.go
@@ -37,7 +37,7 @@ func generateInstructions(instructions []chan instruction) {
 		//The instruction id is equal to the itteration of the for loop.
 		newInstruction.id = i
 		// Randomly generates a new opcode (between 1 and 5) for this instruction
-		newInstruction.opcode = rand.Intn(5)
+		newInstruction.opcode = (rand.Intn(5) + 1)
 		//Sends this new instruction to one of the instructions channels.
 		//The channel that is is sent to is determined by the modulous of it's id. Ensuring that each
 		//execution pipleine is used evenly.
@@ -64,118 +64,113 @@ func executeInstruction(id int, execute <-chan instruction, retire chan<- instru
 	for {
 
 		inst := <-execute
-		//	fmt.Printf("executing instruction %d\n", inst.id)
+		//inst recieves an instruction from the execute channel
+		//If the id of this instruction isn't 101 (not the id of the 'dummy' instruction then)
 		if inst.id != 101 {
-			time.Sleep(time.Second * time.Duration(inst.opcode) / 10)
+			//Sleep for one second multiplied by the number given by the opcode.
+			time.Sleep(time.Second * time.Duration(inst.opcode))
+			//Then pass the instruction into the retire channal.
 			retire <- inst
 		} else {
-			//fmt.Println(id)
+			//Else, it must be the 'dummy' instruction
 			retire <- inst
-			//time.Sleep(time.Second * 10)
+			//pass the dummy instruction into the retire pipleine.
+			//go to the key word f outside of the for loop. Effectivley ending this function
 			goto F
 
 		}
 
 	}
-F:
+F: //Print to show that pipeline has shut down
 	fmt.Println(id, "execution has finished.")
 }
 
 //--------------------------------------------------------------------------------
-// Retires instructions by writing them to the console
+// Pipe sort to ensure instructions are in the correct order
 //--------------------------------------------------------------------------------
 
 func retireInstruction(retired <-chan instruction) {
 
 	for {
-		//retireie := <-retired
-		//fmt.Println(retireie.id)
-		// do forever
-		// Receive an instruction from the generator
-
-		pipeSort := make([]chan instruction, 6) //arrayOfChannels
-
+		//Create Array of 6 instruction channels
+		pipeSort := make([]chan instruction, 6)
+		//setting channels up.
 		for i := 0; i < 6; i++ {
 			pipeSort[i] = make(chan instruction)
 
 		}
+
+		//initalise go routines
 		for i := 0; i < 5; i++ {
 			go pipeSorter(i, pipeSort[i], pipeSort[i+1])
 		}
+		//inatialise retire function and pass in the last pipesort channel
 		go retire(pipeSort[5])
+		//Get the first instruction from the pipeline and give it to the variable myInstruction
 		myInstruction := <-retired
-		//for {
-		//	fmt.Println(myInstruction.id)
-		//	myInstruction = <-retired
-		//}
+
 		for {
-
-			//fmt.Println(myInstruction)
-
-			//select {
-			//case <-retired:
+			//Pass the next instruction from the channel into the next instruction variable.
 			nextInstruction := <-retired
-			//	for {
-			//		fmt.Println(nextInstruction)
-			//		nextInstruction = <-retired
-			//	}
+			//Check to see if the instruction is the dummy instruction
 			if nextInstruction.id == 101 {
+				//If it is the dummy instruction pass the current instruction into the start of the pipesort
 				pipeSort[0] <- myInstruction
+				//Pass the dummy instruction into the pipesort
 				pipeSort[0] <- nextInstruction
+				//Empty the dummy instructions from the other two execute functions.
 				<-retired
 				<-retired
 				goto R
+				//Escape the for loop so this function can end
 			}
-			//for {
-			//	fmt.Println(nextInstruction)
-			//	nextInstruction = <-retired
-			//}
-			//fmt.Println(myInstruction.id)
-			//fmt.Println(nextInstruction.id)
-			//	fmt.Println("Options are ", myInstruction.id, "and  ", nextInstruction.id)
+			//if myInstruction is not the dummy instruction then compare the current instruction
+			//With the next instruction.
 			if myInstruction.id > nextInstruction.id {
+				//If the current instruction is bigger than the next instruction pass the next instruction
+				// into the start of the pipeline sort.
 				pipeSort[0] <- nextInstruction
-				//fmt.Println("i chose ", nextInstruction)
 			} else {
-
-				//	fmt.Println("I chose ", nextInstruction)
+				//Otherwise pass the current instruction into the pipeline sort
 				pipeSort[0] <- myInstruction
+				//Set the current instruction to equal the current next instruction
 				myInstruction = nextInstruction
-				//	}
 
 			}
 
 		}
 
-		///fmt.Printf("Retired: %d \n", opcode.id) // Report to console
 	}
+	//Lable R, so that inner for loop can be escaped when dummy instructions are reached.
 R:
 	fmt.Println("Pipeline entry is complete")
 }
 
 func pipeSorter(id int, myInstructions <-chan instruction, nextInstructions chan<- instruction) {
 
-	//fmt.Println(id, myInstruction.id)
+	//It's current instruction is the first thing passed to it from the instructions channel
 	myInstruction := <-myInstructions
 
 	for {
-
-		//println(id)
-		//select {
-		//case <-myInstructions:
-
+		//It takes its next instruction from the channel of instructions it has to process
 		nextInstruction := <-myInstructions
+		//Check to see if the next instruction is the dummy instruction
 		if nextInstruction.id == 101 {
+			//If it is the dummy instruction it passes its current instruction and the 'dummy' to the next
+			//part of the pipeline via the nextInstructions channel.
 			nextInstructions <- myInstruction
 			nextInstructions <- nextInstruction
+			//Goes to the lable F which will take it out of this for loop and allow the function to end
 			goto F
 		}
-		//fmt.Println(" Pipe sorter My choice is between  ", myInstruction.id, "and  ", nextInstruction.id)
+		//Checks which of the two instructions has the bigger id
 		if myInstruction.id > nextInstruction.id {
-			//fmt.Println("Pipe sorter I CHOSE  ", nextInstruction.id)
+			//If the current instruction has the bigger id it passes the next instruction to the next stage
+			// of the pipeline via the next instructions channel.
 			nextInstructions <- nextInstruction
 		} else {
-			//fmt.Println("pipe sorter I CHOSE  ", myInstruction.id)
+			//if the current instruction is smaller than the next one, pass the current instruction to the next stage
+			//of the pipeline.
 			nextInstructions <- myInstruction
 			myInstruction = nextInstruction
 		}
@@ -185,12 +180,13 @@ F:
 
 func retire(myInstructions <-chan instruction) {
 	for {
-		//<-myInstructions
-
+		//Recieve instructions from the my instruction channel.
 		myInstruction := <-myInstructions
+		//If its the dummy then escape the for loop by going to F
 		if myInstruction.id == 101 {
 			goto F
 		}
+		//Other print out the instruction
 		fmt.Println(myInstruction)
 	F:
 	}
@@ -204,23 +200,17 @@ func retire(myInstructions <-chan instruction) {
 func main() {
 	rand.Seed(time.Now().Unix()) // Seed the random number generator
 
-	// Set up required channel
+	//Create array of channels
 	instructions := make([]chan instruction, 3) //arrayOfChannels
-	//done := make(chan bool)
+	//Set up instruction channels
 	for i := range instructions {
 		instructions[i] = make(chan instruction)
 	}
-
+	//Pass the array of chanels into the generate instruction funtion
+	// and activate it
 	go generateInstructions(instructions)
-	//opcodes := make(chan int) // channel for flow of generated opcodes
-
-	// Now start the goroutines in parallel.
-	fmt.Printf("Start Go routines...\n")
+	//Keep this function going forever
 	for {
-		//select {
-		//case <-done:
-		//goto F
-		//}
 	}
-	//F:
+
 } // end of main /////////////////////////////////////////////////////////////////
